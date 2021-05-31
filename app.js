4735b686014b9e30293fd4648bd21dd44bb1cfd5
KangJuSeong <kjs@kookmin.ac.kr>
2021-05-31 13:09:11 +0900

    make www.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const route = require("./routes/home");
 
-const PORT = 3000;
 const app = express();
 
 // 앱 세팅
@@ -15,6 +14,4 @@ app.set("view engine", "ejs");
 // 미들웨어 등록 메서드 use()
 app.use("/", route);
 
-app.listen(PORT, () => {
-  console.log("Strar Server");
-});
+module.exports = app;
