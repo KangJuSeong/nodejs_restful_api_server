065f00b613493eb1c6d16924e1678d6d05db5e95
KangJuSeong <kjs@kookmin.ac.kr>
2021-05-31 14:00:46 +0900

    Get data

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// html 태그에 있는 id 값을 통해 질의 선택자 설정 가능 (앞에 # 붙여주기)
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("#login");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+};
