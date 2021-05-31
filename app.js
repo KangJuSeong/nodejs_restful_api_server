f748875e8b8337ca2899f7fcefae5837e1ad91b2
KangJuSeong <kjs@kookmin.ac.kr>
2021-05-31 11:01:33 +0900

    ejs로 view 분리

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+// 앱 세팅
+app.set("views", "./views");
+// view 엔진
+app.set("view engine", "ejs");
+
+// 루트
 app.get('/', (req, res) => {
-  console.log('hello');
-  res.json({"hello": "hello"});
+  console.log('home');
+  res.render("home/index");
 });
+// 로그인
+app.get('/login', (req, res) => {
+  console.log('login');
+  res.render("home/login");
+})
 
 app.listen(3000, () => {
   console.log("Strar Server");
