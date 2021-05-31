07e01d6e9fd6e0b7284fecf99c8b98e9257ce79e
KangJuSeong <kjs@kookmin.ac.kr>
2021-05-31 13:47:13 +0900

    Connect Front and JS

--- app/app.js
+++ app/app.js
@@ -13,6 +13,8 @@ app.set("view engine", "ejs");
 
 // 미들웨어 등록 메서드 use()
 app.use("/", route);
+// express.static 을 이용하여 정적 경로를 지정
+app.use(express.static(`${__dirname}/src/public`));
 
 // app 내보내기
 module.exports = app;
