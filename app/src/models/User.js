f7c0e25e491ef20df3b0ac8a621c0d4fd1ae2a51
KangJuSeong <kjs@kookmin.ac.kr>
2021-06-01 13:48:23 +0900

    code refactoring

---
+++ app/src/models/User.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const UserStorage = require("./UserStorage");
+
+class User {
+  constructor(body) {
+    this.body = body;
+  }
+
+  login() {
+    const body = this.body;
+    const info = UserStorage.getUserInfo(body.id);
+    if (!info.id) {
+      return { success: false, message: "존재하지 않는 계정" };
+    }
+    if (info.id === body.id && info.psword === body.psword) {
+      return { success: true };
+    }
+    else {
+      return { success: false, message: "비밀번호 불일치" };
+    }
+  }
+}
+
+module.exports = User;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,6 +2,7 @@
 
 const e = require("express");
 const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -16,27 +17,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-    psword = req.body.psword;
-    const users = UserStorage.getUsers("id", "psword");
-    const response = {};
-    if(users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if(users.psword[idx] == psword) {
-        response.success = true;
-        return res.json(response);
-      }
-      else {
-        response.success = false;
-        response.message = "비밀번호 불일치";
-        return res.json(response);
-      };
-    }
-    else {
-      response.success = false;
-      response.message = "존재하지 않는 계정";
-      return res.json(response);
-    };
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
   }
 };
 
